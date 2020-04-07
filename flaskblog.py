7a79ba87534d8651ebae9fab7b6da2347a226ac5
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-04-08 01:41:24 +0530

    added templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Abhishek Maira',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 8, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 9, 2020'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
+
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)    
+    app.run(debug=True)
