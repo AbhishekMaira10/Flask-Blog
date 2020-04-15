d69c7cf6c7d414c4ff7f14bc07f8fc345b712420
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-04-15 23:44:50 +0530

    added accounts page and fixed login bug

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -16,7 +16,9 @@ app.config['SECRET_KEY'] = '6fa73538fe5688211055ed30dd556b17'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)   
-login_manager = LoginManager(app) 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
 
