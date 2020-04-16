41e6ca085316f8e25dd12d12b3f7e7bd69962365
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-04-16 20:45:01 +0530

    new post feature

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -4,7 +4,7 @@ from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.models import User, Post
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 
@@ -110,3 +110,14 @@ def account():
         'static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home '))
+    return render_template('create_post.html', title='New Post', form=form)
+
