c57f9f71083f71cb794bd7f3722c28f2dc20718b
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-04-17 18:03:48 +0530

    update and delete post feature

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -139,3 +139,16 @@ def update_post(post_id):
                            form=form, legend='Update Post')
 
 
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
+    
+
