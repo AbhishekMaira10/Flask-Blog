2899f548733fba97ce146479c234dca454696503
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-04-17 20:28:40 +0530

    code for latest posts and posts of a particular user

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -6,13 +6,14 @@ from flaskblog import app, db, bcrypt
 from flaskblog.models import User, Post
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_login import login_user, current_user, logout_user, login_required
- 
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    page = request.args.get('page, 1', type=int)
-    posts = Post.query.paginate(per_page=5, page=page)  
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(
+        Post.date_posted.desc()).paginate(per_page=5, page=page)
     return render_template('home.html', posts=posts)
 
 
@@ -64,14 +65,15 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(
+        app.root_path, 'static/profile_pics', picture_fn)
 
     output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
-  
+
     i.save(picture_path)
-    
+
     return picture_fn
 
 
@@ -86,9 +88,9 @@ def account():
         current_user.username = form.username.data
         current_user.eamil = form.email.data
         db.session.commit()
-        flash('Your account has been updated!','success')
+        flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
-    
+
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
@@ -103,13 +105,14 @@ def account():
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        post = Post(title=form.title.data,
+                    content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post',
-                            form=form, legend='New Post')
+                           form=form, legend='New Post')
 
 
 @app.route("/post/<int:post_id>")
@@ -126,12 +129,12 @@ def update_post(post_id):
         abort(403)
     form = PostForm()
     if form.validate_on_submit():
-       post.title = form.title.data
-       post.content = form.content.data
-       db.session.commit()
-       flash('Your post has been updated!', 'success')
-       return redirect(url_for('post', post_id=post.id))
-    
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+
     elif request.method == 'GET':
         form.title.data = post.title
         form.content.data = post.content
@@ -146,10 +149,18 @@ def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
         abort(403)
-    
+
     db.session.delete(post)
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
-    
 
+
+@app.route('/user/<string:username>')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(per_page=5, page=page)
+    return render_template('user_posts.html', posts=posts, user=user)
