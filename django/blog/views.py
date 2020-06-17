f12267cfd408b7f810507fac91fd73ecf7a660cb
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-06-17 09:58:51 +0530

    add starter templates and static file

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Abhishek Maira',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 17 2020'
+    },
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 18 2020'
+    },
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
